a1a16396fb11791cfeff62706b57b53cf3953d1e
syedmehedi0003 <mehedihasanbahar.com>
2022-04-19 02:22:13 +0600

    new commit

---
+++ src/Pages/Shared/Loading/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+
+const Loading = () => {
+    return (
+        <div style={{ width: '300px' }} className='w-100 d-flex justify-content-center align-items-center'>
+            <Spinner animation="border" variant="danger" />
+        </div>
+    );
+};
+
+export default Loading;
